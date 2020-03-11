d74139a2c090909b59981f3127deab6e3aa131a0
yaogengzhu <455947455@qq.com>
2020-03-11 21:20:34 +0800

    fix

--- note/node/test/01.js
+++ note/node/test/01.js
@@ -1,14 +1,30 @@
 // console.log(global)
 
 // console.log(process.env)
-process.env.NODE_ENV = 'dev'
+// process.env.NODE_ENV = 'dev'
 
-console.log(process.env.NODE_ENV)
-let url = ''
-if (!process.env.NODE_ENV) {
-    url = 'localhost:3000'
-} else {
-    url = 'yaogeng.top'
+// console.log(process.env.NODE_ENV)
+// let url = ''
+// if (!process.env.NODE_ENV) {
+//     url = 'localhost:3000'
+// } else {
+//     url = 'yaogeng.top'
+// }
+
+// console.log(url)
+
+function f2() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(333)   
+        }, 0);
+    })
 }
 
-console.log(url)
+async function f1() {
+    const result = await f2()
+    console.log(result)
+    console.log('await')
+}
+
+console.log(f1())
