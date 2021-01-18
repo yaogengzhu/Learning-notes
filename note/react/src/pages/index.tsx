a7e4509fa660fc81e2a9015fad3476a56ae73521
yaogengzhu <455947455@qq.com>
2021-01-18 14:09:00 +0800

    save

--- note/react/src/pages/index.tsx
+++ note/react/src/pages/index.tsx
@@ -1,54 +1,17 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react'
-import { useSerachBooks } from './serach'
+import React, { useEffect } from 'react'
 
 export default () => {
-	const [query, setQuery] = useState('')
-	const [pageNumber, setPageNumber] = useState(1)
-
-	const {loading, books, hasMore, error} = useSerachBooks(query, pageNumber)
-
-
-	const observer = useRef()
-
-	const lastBooksEelmentRef = useCallback((node) => {
-		if (loading) return
-		if (observer.current) {
-			observer.current.disconnect()
-		}
-		console.log('node', node)
-		observer.current = new IntersectionObserver((entries) => {
-			if (entries[0].isIntersecting && hasMore) {
-				console.log('visiable')
-				setPageNumber( (prveNumber) => prveNumber + 1)
-			}
-		})
-		if (node) observer.current.observe(node)
-		console.log(node, 'node')
-	}, [loading, hasMore])
-	
+	useEffect(() => {
+		console.log("%c hello", "background:#fff; color: red" )
+		console.warn("%c 希望盗链的大佬手下留情 ","background:#24272A; color:#ffffff","","https://cangshui.net/");
+	}, [])
 	return (
 		<div>
-			<input
-				type='text'
-				value={query}
-				onChange={(e) => {
-				setQuery(e.target.value)
-					setPageNumber(1)
-				}}
-			>
-
-			</input>
 			{
-				books.map( (book: any, index: number) => {
-					if (books.length === index + 1) {
-						return <div key={ book } ref={ lastBooksEelmentRef }>{ book}</div>
-					} else {
-						return <div key={ book }>{ book}</div>
-					}
-				})
+				Array(3000).fill('').map((_, i) => (
+					<div key={i}>{ i }</div>
+				))
 			}
-			<div>{ loading && 'Loading...'}</div>
-			<div>{ error && 'Error'}</div>
 		</div>
 	)
 }
--- note/react/src/pages/main/main.tsx
+++ note/react/src/pages/main/main.tsx
@@ -1,4 +1,3 @@
-import { Button, TextField } from '@material-ui/core'
 import React from 'react'
 import { Formik } from 'formik'
 
