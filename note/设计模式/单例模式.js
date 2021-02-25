bd8092fb895043cc99b5f4d176617c5de6856eb7
yaogengzhu <455947455@qq.com>
2021-02-25 22:37:40 +0800

    单例模式

---
+++ note/设计模式/单例模式.js
@@ -0,0 +1,43 @@
+// 单例子模式 保证一个类中只有一个实例
+/**
+ *  使用场景，全局的蒙层
+ */
+
+class Window {
+    constructor(name) {
+        this.name = name
+    }
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new Window()
+        }
+
+        return this.instance
+    }
+}
+
+let w1 = Window.getInstance()
+let w2 = Window.getInstance()
+
+console.log(w1 === w2)
+
+
+function Singleton() {
+    //
+}
+
+Singleton.getInstance = function (...args) {
+    if (!Singleton.instance) {
+        Singleton.instance = new Singleton()
+    }
+
+    return Singleton.instance
+}
+
+Singleton.instance = null
+
+const a = Singleton.getInstance()
+const b = Singleton.getInstance()
+
+console.log(a === b)
