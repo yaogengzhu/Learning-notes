754aa917b059cd6a773c23511fc4d8a3c5541da4
yaogengzhu <455947455@qq.com>
2021-03-24 00:02:58 +0800

    虚拟滚动

--- note/react/src/pages/index.tsx
+++ note/react/src/pages/index.tsx
@@ -1,17 +1,52 @@
-import React, { useEffect } from 'react'
-
+import React,{ useEffect } from 'react'
+import { FixedSizeList as List } from 'react-window'
+import AutoSizer from "react-virtualized-auto-sizer"
+import './index.less'
+import axios from 'axios'
+import { json } from '@/config/color'
 export default () => {
-	useEffect(() => {
-		console.log("%c hello", "background:#fff; color: red" )
-		console.warn("%c 希望盗链的大佬手下留情 ","background:#24272A; color:#ffffff","","https://cangshui.net/");
-	}, [])
-	return (
-		<div>
-			{
-				Array(3000).fill('').map((_, i) => (
-					<div key={i}>{ i }</div>
-				))
-			}
-		</div>
-	)
+
+    useEffect(() => {
+        console.log("%c 一起进步 ","background:#14cc9b; color:#ffffff","","https://github.com/yaogengzhu")
+
+    },[])
+
+    const getList = () => {
+        axios.post('http://zhongguose.com/colors.json')
+            .then(res => {
+                console.log(res)
+            })
+    }
+    const Row = ({ index,style }) => {
+        const currentList = json[ index ]
+        return (<div style={ { backgroundColor: currentList.hex,color: '#fff',marginBottom: '10px',...style } } className='item' key={ currentList.name } >
+            <span>{ currentList.name }</span>
+            <span>这是啥</span>
+        </div>)
+    }
+
+    const Example = () => (
+        <AutoSizer>
+            {({ height,width }) => (
+                <List
+                    height={ height }
+                    itemCount={ json.length }
+                    itemSize={ 100 }
+                    width={ width }
+                >
+                    { Row }
+                </List>
+            ) }
+        </AutoSizer>
+
+    )
+
+    return (
+        <div>
+            <h1>虚拟列表</h1>
+            <div className='box'>
+                <Example />
+            </div>
+        </div>
+    )
 }
