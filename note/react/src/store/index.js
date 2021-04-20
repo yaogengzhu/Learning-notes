8a7ec61cdfbc862598a618cef19b39921578a02b
朱耀更 <zhuyaogeng@b-and-qchina.com>
2021-04-20 23:20:45 +0800

    自定义hooks

---
+++ note/react/src/store/index.js
@@ -0,0 +1,12 @@
+// 要求匹配 #ffbbad #FC01DF #FFE #ffE
+
+const regx = /#(\w{6}|\w{3})/g
+
+const string = '#ffbaxd #Fc0_dF #ff3 #ff3e'
+
+const str = string.match(regx)
+console.log(str)
+
+// 匹配时间
+
+// 23:22 18:00
