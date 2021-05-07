c4c781d192e2d19cf63dd46de9d5f64705f63760
yaogengzhu <455947455@qq.com>
2021-05-07 23:46:40 +0800

    SVAE

---
+++ note/react/src/pages/index.jsx
@@ -0,0 +1,29 @@
+/*
+ * @Author: yaogeng.zhu 
+ * @Date: 2021-04-24 13:30:24 
+ * @Last Modified by: yaogeng.zhu
+ * @Last Modified time: 2021-04-24 18:16:16
+ */
+import React, { useEffect } from 'react'
+import jsonp from 'jsonp'
+
+const App = () => {
+	useEffect(() => {
+		getData()
+	
+	}, [])
+
+	const  getData = async () => {
+		const url = 'https://u.y.qq.com/cgi-bin/musics.fcg?-=getUCGI27221538482173435&g_tk=5381&sign=zza1hcb4ned455pzp02f3fef04bda3a286eeb40136f6b80f4a&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq.json&needNewCode=0&data=%7B%22comm%22%3A%7B%22ct%22%3A24%2C%22cv%22%3A0%7D%2C%22singerList%22%3A%7B%22module%22%3A%22Music.SingerListServer%22%2C%22method%22%3A%22get_singer_list%22%2C%22param%22%3A%7B%22area%22%3A-100%2C%22sex%22%3A-100%2C%22genre%22%3A-100%2C%22index%22%3A-100%2C%22sin%22%3A0%2C%22cur_page%22%3A1%7D%7D%7D'
+	 	jsonp(url, null, (err, data) => {
+			const { singerlist } = data.singerList.data
+			console.log(singerlist, '??')
+		})
+		
+	}
+	return (
+		<div>测试</div>
+	)
+}
+
+export default App
---
+++ note/react/src/utils/index.js
@@ -0,0 +1,19 @@
+export function jsonp(url) {
+    console.log(url, '>')
+    return new Promise((resolve, reject) => {
+            // 这里的 "jsonCallBack" ，和调用的 jsonp 的 url 中的 callback 值相对应（见粗体字）
+            window.jsonCallBack =(result) => {
+                console.log(result, '???/')
+                resolve(result)
+            }
+            const JSONP = document.createElement('script');
+            JSONP.type = 'text/javascript';
+            JSONP.src = url;
+            document.getElementsByTagName('head')[0].appendChild(JSONP);
+            setTimeout(() => {
+                document.getElementsByTagName('head')[0].removeChild(JSONP)
+            },500)
+
+        })
+}
+
