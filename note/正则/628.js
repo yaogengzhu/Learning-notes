a18746c9bb6edd660ddc37bb058308d2e2f94adf
yaogengzhu <455947455@qq.com>
2021-06-28 16:17:06 +0800

    分组引用

--- note/正则/628.js
+++ note/正则/628.js
@@ -13,3 +13,13 @@ const regx2 = /(ab)+/g
 const regx3 = /^I love (javascript|java)$/g
 
 // 3.分组引用
+const regx4 = /(\d{4})-(\d{2})-(\d{2})/g
+'2021-06-29'.replace(regx4, function(match, year, month, day) {
+    console.log(match, year, month, day)
+})
+
+/**
+ *  匹配 2021-12-12 2021/12/12 2021.12.12
+ * 
+ */
+const regx5 = /^\d{4}(\/|\.|-)\d{2}\1\d{2}$/
