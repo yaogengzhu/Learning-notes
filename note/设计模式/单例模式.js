3dce4df5d991e6ebebc52898392f67c7e5647f8b
yaogengzhu <455947455@qq.com>
2021-09-10 10:06:40 +0800

    单例模式

--- note/设计模式/单例模式.js
+++ note/设计模式/单例模式.js
@@ -5,23 +5,22 @@
 
 class Window {
     constructor(name) {
-        this.name = name
+        this.name = name;
     }
 
     static getInstance() {
         if (!this.instance) {
-            this.instance = new Window()
+            this.instance = new Window();
         }
 
-        return this.instance
+        return this.instance;
     }
 }
 
-let w1 = Window.getInstance()
-let w2 = Window.getInstance()
+let w1 = Window.getInstance();
+let w2 = Window.getInstance();
 
-console.log(w1 === w2)
-
+console.log(w1 === w2);
 
 function Singleton() {
     //
@@ -29,31 +28,54 @@ function Singleton() {
 
 Singleton.getInstance = function (...args) {
     if (!Singleton.instance) {
-        Singleton.instance = new Singleton()
+        Singleton.instance = new Singleton();
     }
 
-    return Singleton.instance
-}
+    return Singleton.instance;
+};
 
-Singleton.instance = null
+Singleton.instance = null;
 
-const a = Singleton.getInstance()
-const b = Singleton.getInstance()
+const a = Singleton.getInstance();
+const b = Singleton.getInstance();
 
-console.log(a === b)
+console.log(a === b);
 
 const Singleton1 = (function () {
-    let instance = null
+    let instance = null;
     return function () {
         if (instance) {
-            return instance
+            return instance;
         }
 
-        return instance = this
-    }
-})()
+        return (instance = this);
+    };
+})();
 
-const c = new Singleton1()
-const d = new Singleton1()
+const c = new Singleton1();
+const d = new Singleton1();
 
-console.log(c === d)
+console.log(c === d);
+
+const CreateDiv = (function () {
+    let instance;
+    let CreateDiv = function (html) {
+        if (instance) {
+            return instance;
+        }
+        this.html = html;
+        this.init();
+        return (instance = this);
+    };
+
+    CreateDiv.prototype.init = function () {
+        const div = document.createElement('div');
+        div.innerHTML = this.html;
+        document.body.appendChild(div);
+    };
+    return CreateDiv;
+})();
+
+var e = new CreateDiv('sven1');
+var f = new CreateDiv('sven2');
+console.log( a === b)
