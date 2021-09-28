b68e518cd0b19b03f4e154487e137ccdcd8e9415
yaogengzhu <455947455@qq.com>
2021-09-28 09:41:11 +0800

    对new实现原理的补充

--- note/手写代码/new的实现原理.js
+++ note/手写代码/new的实现原理.js
@@ -11,7 +11,7 @@ function Person() {
     this.name = 'zs'
 }
 
-Person.prototype.say = function() {
+Person.prototype.say = function () {
     console.log(this.name)
 }
 // const p = new Person()
@@ -24,8 +24,15 @@ function _new() {
     const constructor = Array.prototype.shift.call(arguments) // 取出构造函数
     obj.__proto__ = constructor.prototype // 对象继承构造函数原型
     const result = constructor.apply(obj, arguments) // 改变this指向 ----> 对象的this指向构造函数 为继承构造函数的属性和方法
-    return obj // 返回一个对象
+    return result instanceof Object ? result : obj // 返回一个对象
 }
+
+/**
+ * 1.new关键字会首先创建一个空对象
+ * 2.将这个空对象的原型对象指向构造函数的原型属性，从而继承原型上的方法
+ * 3.将this指向这个空对象，执行构造函数中的代码，以获取私有属性
+ * 4.如果构造函数返回了一个对象res，就将该返回值res返回，如果返回值不是对象，就将创建的对象返回
+ */
 
 
 const p1 = _new(Person)
@@ -34,7 +41,7 @@ p1.say()
 
 const arr = [1, 3, 3, null, undefined, NaN]
 
-const arr1 = arr.filter( item => !isNaN(item) )
+const arr1 = arr.filter(item => !isNaN(item))
 Number.isNaN(undefined) // false
 isNaN(undefined) // true
 console.log(arr1)
