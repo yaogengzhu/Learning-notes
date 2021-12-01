9bfb91b05abea4eeec4bdef2d9d79d81bdea16d5
yaogengzhu <455947455@qq.com>
2021-12-01 10:19:26 +0800

    continue语句

--- note/es6/index.js
+++ note/es6/index.js
@@ -1,38 +1,21 @@
-const METHOD_NAME = 'm'
-const symbol = Symbol()
-let weirdMethods = {
-    'method with spaces'(x){ return x + 1},
-    [METHOD_NAME](x){ return x + 2},
-    [symbol](x){return x + 3}
-}
-
-console.log(weirdMethods["method with spaces"](1))
-console.log(weirdMethods[METHOD_NAME](1))
-console.log(weirdMethods[symbol](1))
-
-
-let p = {
-    x: 1.0,
-    y: 1.0,
-    // get r() {
-    //     return Math.hypot(this.x, this.y)
-    // },
-    // set r(newValue) {
-    //     let oldValue = Math.hypot(this.x, this.y)
-    //     let ratio = newValue / oldValue
-    //     this.x *= ratio
-    //     this.y *= ratio
-    // },
-    get theta() {
-        return Math.atan2(this.y, this.x)
+function a() {
+    for (let i = 0; i < 10; i++) {
+        if (i === 4) {
+            // continue
+            return;
+        }
+        console.log(i);
     }
 }
+a()
+{
+    const arr = Array.from({ length: 10 }, (v, i) => i + 1);
+    console.log(arr)
 
-console.log(p.x)
-console.log(p.theta)
-
-const q = Object.create(p)
-q.x =  3
-q.y = 4
-
-console.log(q.theta)
+    arr.forEach((v) => {
+        if (v === 4) {
+            return;
+        }
+        console.log(v);
+    });
+}
