36f49cb6975ce4ec5a3d906e18f6204c07864269
yaogengzhu <455947455@qq.com>
2021-12-15 17:43:49 +0800

    review

--- note/手写代码/节流防抖.js
+++ note/手写代码/节流防抖.js
@@ -1,24 +1,43 @@
-function throttle() {
-    let timer = null
+/*
+ * @Author: yaogeng.zhu
+ * @Date: 2021-12-15 17:38:40
+ * @Last Modified by: yaogeng.zhu
+ * @Last Modified time: 2021-12-15 17:43:23
+ */
+
+/**
+ * 多次点击，间隔多少时间执行一次
+ * @param {*} fn
+ * @param {*} delay
+ * @returns
+ */
+function throttle(fn, delay) {
+    let timer = null;
     return function () {
-        if (timer) return
-        const args = Array.prototype.slice.call(arguments)
-        timer = setinterval(() => {
-            fn.call(this, ...args)
-            timer = null
-        }, delay)
-    }
+        if (timer) return;
+        const args = Array.prototype.slice.call(arguments);
+        timer = setTimeout(() => {
+            fn.call(this, ...args);
+            timer = null;
+        }, delay);
+    };
 }
 
+/**
+ * 点击多次，只执行一次，如果定时器存在，直接清空定时器
+ * @param {*} fn
+ * @param {*} delay
+ * @returns
+ */
 function debounce(fn, delay) {
-    let timer = null
+    let timer = null;
     return function () {
         if (timer) {
-            clearInterval(timer)
+            clearInterval(timer);
         }
-        const args = Array.prototype.slice.call(arguments)
-        timer = setinterval(() => {
-            fn.call(this, ...args)
-        }, delay)
-    }
+        const args = Array.prototype.slice.call(arguments);
+        timer = setTimeout(() => {
+            fn.call(this, ...args);
+        }, delay);
+    };
 }
