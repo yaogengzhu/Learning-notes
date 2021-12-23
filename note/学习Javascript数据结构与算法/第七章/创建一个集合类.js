9f6a6186f4b3e584a807936b22f8f230b1302bd8
龙风 <455947455@qq.com>
2021-12-23 18:35:10 +0800

    实现集合类

---
+++ note/学习Javascript数据结构与算法/第七章/创建一个集合类.js
@@ -0,0 +1,61 @@
+class Set {
+    constructor() {
+        this.items = {}
+    }
+
+    add(element) {
+        if (!this.has(element)) {
+            this.items[element] = element
+            return true
+        }
+        return false
+    }
+
+    delete(element) {
+        if (!this.has(element)) {
+            delete this.items[element]
+            return true
+        }
+        return false
+    }
+
+    // 第一个实现，添加和删除，其他方法都需要首先判断这个数是否存在对象中。唯一性
+    has(element) {
+        // return element in this.items
+
+        // 方式1
+        // return this.items.hasOwnProperty(element)
+        /**
+         * 方式2 --> 解决了不是所有的对象都继承了Object.prototype
+         * 也有可能hasOwnProperty的方法被覆盖了，避免出现任何问题，使用Object.prototype.hasOwnProperty.call 是更安全的做法
+         */
+        return Object.prototype.hasOwnProperty.call(this.items, element)
+    }
+
+    clear() {
+        this.items = {}
+    }
+
+    size() {
+        return Object.keys(this.items).length
+    }
+
+    values() {
+        return Object.values(this.items)
+    }
+}
+
+const set = new Set()
+set.add(1)
+set.add(2)
+
+// 额外的知识点补充 获取对象的长度
+function getObjectSize(obj) {
+    let count = 0
+    for (let key in obj) {
+        if (Object.prototype.hasOwnProperty.call(obj, key)) {
+            count++
+        }
+    }
+    return count
+}
