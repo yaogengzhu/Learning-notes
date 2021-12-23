ce250771f56de4731acce1bc238249b0d6554fe1
龙风 <455947455@qq.com>
2021-12-23 17:08:48 +0800

    策略模式补充学习

--- note/设计模式/策略模式.js
+++ note/设计模式/策略模式.js
@@ -45,3 +45,77 @@ var bonus = new Bonus()
 bonus.setSalary(1000)
 bonus.setStrategy(new performaceS())
 console.log(bonus.getBonus())
+
+/**
+ *  javaScript的实现方式
+ */
+
+const strategyObj = {
+    S: function (salary) {
+        return salary * 4
+    },
+    A: function (salary) {
+        return salary * 3
+    },
+}
+
+function getBonusByStrategy(type, salary) {
+    return strategyObj[type](salary)
+}
+console.log(getBonusByStrategy('A', 1000))
+
+/**
+ * 表单验证策略模式
+ *
+ */
+
+// 策略对象
+const strategyForm = {
+    isNotEmpty: function (value, errorMsg) {
+        if (value === '') {
+            return errorMsg
+        }
+    },
+}
+
+// 环境类：
+
+const validForm = function () {
+    this.cache = [] // 保存有效规则
+}
+
+validForm.prototype.add = function (dom, rule, erroMsg) {
+    const str = rule.split(':')
+    this.cache.push(function () {
+        let strategy = str.shift() // 取出第一个策略模式
+        str.unshift(dom.value) // 把input的值驾到参数列表中
+        str.push(erroMsg) // 把错误信息添加到参数列表中
+        return strategyForm[strategy].apply(dom, str)
+    })
+}
+
+validForm.prototype.start = function () {
+    for (let i = 0; valildFunc; valildFunc = this.cache[i++]) {
+        const msg = valildFunc()
+        if (msg) {
+            return msg
+        }
+    }
+}
+
+var validataFunc = function () {
+    var validator = new validForm() // 创建一个 validForm 对象
+    validator.add(registerForm.userName, 'isNonEmpty', '用户名不能为空')
+    validator.add(registerForm.password, 'minLength:6', '密码长度不能少于 6 位')
+    validator.add(registerForm.phoneNumber, 'isMobile', '手机号码格式不正确')
+    var errorMsg = validator.start() // 获得校验结果
+    return errorMsg // 返回校验结果
+}
+var registerForm = document.getElementById('registerForm')
+registerForm.onsubmit = function () {
+    var errorMsg = validataFunc() // 如果 errorMsg 有确切的返回值，说明未通过校验
+    if (errorMsg) {
+        alert(errorMsg)
+        return false // 阻止表单提交
+    }
+}
