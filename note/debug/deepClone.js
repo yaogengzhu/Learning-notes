1342843ef0f1eade7729de15e7783325aafe5adc
yaogengzhu <455947455@qq.com>
2022-01-06 09:36:29 +0800

    复习手写深拷贝

--- note/debug/deepClone.js
+++ note/debug/deepClone.js
@@ -1,27 +1,36 @@
-function deepClone(origin, hasMap = new WeakMap()) {
-    if (typeof origin !== 'object' || typeof origin === 'undefined') {
+function deeepClone(origin, hasMap = new WeakMap()) {
+    if (typeof origin !== 'object' || typeof origin === 'undefined' || origin === null) {
         return origin
     }
-
+    if (origin instanceof Date) {
+        return new Date(origin)
+    }
+    if (origin instanceof RegExp) {
+        return new RegExp(origin)
+    }
+    const hasKey = hasMap.get(origin)
+    if (hasKey) {
+        return hasKey
+    }
     let target = new origin.constructor()
     hasMap.set(origin, target)
-    for (let k in origin) {
-        if (Object.prototype.hasOwnProperty.call(origin, k)) {
-            target[k] = deepClone(origin[k], hasMap)
+    for (let key in origin) {
+        if (Object.prototype.hasOwnProperty.call(origin, key)) {
+            target[key] = deeepClone(origin[key], hasMap)
         }
     }
-
     return target
 }
 
 const obj = {
-    name: 'zs',
-    city: {
-        town: '武汉三镇',
+    a: 1,
+    b: {
+        city: 'najing'
     },
+    c: null
 }
 
-const obj1 = deepClone(obj)
-obj1.city.town = '阳新'
+const obj1 = deeepClone(obj)
+obj1.b.city = 'wuhan'
 console.log(obj1)
 console.log(obj)
