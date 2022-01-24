f62e0ce4f194c2c2e606d3eb24a44c9586f0d142
yaogengzhu <455947455@qq.com>
2022-01-24 19:18:11 +0800

    add: 双向链表

--- note/学习Javascript数据结构与算法/第六章/双向链表.js
+++ note/学习Javascript数据结构与算法/第六章/双向链表.js
@@ -1,14 +1,41 @@
-class DoublyNode extends Node {
-    // {1}
-    constructor(element, next, prev) {
-        super(element, next); // {2}
-        this.prev = prev; // {3} 新增的 }
+/**
+ * 双向链表
+ */
+function DobulyLinkList() {
+    function Node(data) {
+        this.data = data
+        thisl.prev = null
+        this.next = null
     }
+    this.head = null
+    this.tail = null
+    this.length = 0
 }
-class DoublyLinkedList extends LinkedList {
-    // {4}
-    constructor(equalsFn = defaultEquals) {
-        super(equalsFn); // {5}
-        this.tail = undefined; // {6} 新增的 }
-    }
-}
+
+// 尾部添加一个新的项目
+DobulyLinkList.prototype.append = function () {}
+
+// 向列表指定位置添加一个新的项
+DobulyLinkList.prototype.insert = function (position, element) {}
+
+// 获取对应的位置信息
+DobulyLinkList.prototype.get = function () {}
+
+DobulyLinkList.prototype.indexOf = function () {}
+
+DobulyLinkList.prototype.update = function () {}
+
+DobulyLinkList.prototype.removeAt = function (position, element) {}
+
+// 从列表中移除一项
+DobulyLinkList.prototype.remove = function (elment) {}
+
+DobulyLinkList.prototype.isEmpty = function () {}
+
+DobulyLinkList.prototype.size = function () {}
+
+DobulyLinkList.prototype.toString = function () {}
+
+DobulyLinkList.prototype.forwardString = function () {}
+
+DobulyLinkList.prototype.backwordString = function () {}
