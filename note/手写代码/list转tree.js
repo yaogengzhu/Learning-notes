5747a0d79782e665983fc2ee3ac516be9fd44d6e
yaogengzhu <455947455@qq.com>
2022-03-07 13:27:48 +0800

    list转tree

---
+++ note/手写代码/list转tree.js
@@ -0,0 +1,27 @@
+function listTransfromToTree(list) {
+    const tree = []
+    for (const node of list) {
+        // 如果没有pid就可以认为是根节点
+        if (node.parentId === '0') {
+            let p = { ...node }
+            p.children = getChildren(p.id, list)
+            tree.push(p)
+        }
+    }
+    function getChildren(id, list) {
+        const children = []
+        for (const node of list) {
+            if (node.parentId === id) {
+                children.push(node)
+            }
+        }
+        for (const node of children) {
+            const nextChildren = getChildren(node.id, list)
+            if (nextChildren.length) {
+                node.children = nextChildren
+            }
+        }
+        return children
+    }
+    return tree
+}
