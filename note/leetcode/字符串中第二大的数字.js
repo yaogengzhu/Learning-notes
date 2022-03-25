65ca1d2eb775adfcee705fd002ce89a29d8215be
yaogengzhu <455947455@qq.com>
2022-03-25 12:47:23 +0800

    add: leetcode

---
+++ note/leetcode/字符串中第二大的数字.js
@@ -0,0 +1,20 @@
+/***
+ *  给你一个混合字符串 s ，请你返回 s 中 第二大 的数字，如果不存在第二大的数字，请你返回 -1 。
+ *  输入：s = "dfa12321afd"
+ *  输出：2
+ *  解释：出现在 s 中的数字包括 [1, 2, 3] 。第二大的数字是 2 。
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const secondHighest = function (s) {
+    let arr = s.match(/\d/g)
+    arr = [...new Set([...arr])]
+    console.log(arr)
+    if (arr.length === 0 || arr.length === 1) return -1
+    const newArr = arr.filter((item) => +item !== Math.max.apply(null, arr))
+    if (newArr.length === 1) return newArr[0]
+    return Math.max.apply(null, newArr)
+}
+
