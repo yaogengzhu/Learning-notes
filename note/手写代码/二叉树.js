ae9568f3ba575e4af4dd1827c94a158e63466be5
yaogengzhu <455947455@qq.com>
2022-03-31 09:36:11 +0800

    upade: binaryTree

--- note/手写代码/二叉树.js
+++ note/手写代码/二叉树.js
@@ -1,55 +1,81 @@
 class BinaryTree {
     constructor() {
-        this.root = null;
+        this.root = null
         // 节点构造函数
         this.Node = function Node(key) {
-            this.key = key;
-            this.left = null;
-            this.right = null;
-        };
+            this.key = key
+            this.left = null
+            this.right = null
+        }
     }
     // 生成二叉树
     buildTree(list) {
-        list = list.filter((item) => item !== null && item !== undefined);
+        list = list.filter((item) => item !== null && item !== undefined)
         // 将每个数据变成节点，放入数组中
-        const nodeList = [];
-        const len = list.length;
+        const nodeList = []
+        const len = list.length
         for (let i = 0; i < len; i++) {
-            nodeList[i] = new this.Node(list[i]);
+            nodeList[i] = new this.Node(list[i])
         }
-        this.root = nodeList[0];  // 取出第一个节点作为根节点
-         // 分配节点
+        this.root = nodeList[0] // 取出第一个节点作为根节点
+        // 分配节点
         for (let i = 0; i < len / 2; i++) {
             if (2 * i + 1 < len && nodeList[2 * i + 1]) {
-                nodeList[i].left = nodeList[2 * i + 1];
+                nodeList[i].left = nodeList[2 * i + 1]
             }
             if (2 * i + 2 < len && nodeList[2 * i + 2]) {
-                nodeList[i].right = nodeList[2 * i + 2];
+                nodeList[i].right = nodeList[2 * i + 2]
             }
         }
     }
 
     findTargetNode(node, targetSum) {
         if (node === null) {
-            return false;
+            return false
         }
         if (node.key > targetSum) {
-            return false;
+            return false
         }
         if (node.key === targetSum) {
-            return true;
+            return true
         }
         return (
             this.findTargetNode(node.left, targetSum - node.key) ||
             this.findTargetNode(node.right, targetSum - node.key)
-        );
+        )
+    }
+
+    preOrder(root) {
+        if (root !== null) {
+            console.log(root.key)
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+        }
+    }
+
+    centerOder(root) {
+        if (root !== null) {
+            this.preOrder(root.left)
+            console.log(root.key)
+            this.preOrder(root.right)
+        }
+    }
+    lastOrder(root) {
+        if (root !== null) {
+            this.preOrder(root.left)
+            this.preOrder(root.right)
+            console.log(root.key)
+        }
     }
 }
 
-const tree = new BinaryTree();
-tree.buildTree([null, 1, 2, 4, 6, 8, 10, 133]);
+const tree = new BinaryTree()
+tree.buildTree([null, 1, 2, 4, 6, 8, 10, 133])
 // console.log(tree.root);
 const result = tree.findTargetNode(tree.root, 10)
-console.log('====================================');
-console.log(result);
-console.log('====================================');
+console.log('====================================')
+console.log(result)
+console.log('====================================')
+tree.preOrder(tree.root)
+tree.centerOder(tree.root)
+tree.lastOrder(tree.root)
