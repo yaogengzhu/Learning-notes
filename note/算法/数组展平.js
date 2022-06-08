83a5ef5c74a19e1e16f6185943adc5032a2d0ea0
yaogengzhu <455947455@qq.com>
2022-06-08 13:33:27 +0800

    .

--- note/算法/数组展平.js
+++ note/算法/数组展平.js
@@ -2,13 +2,21 @@
  *  多维数组展平
  *  递归方式实现
  */
-const arr =  [[1,34,5, [2,4], [[1,4]]]]
+const arr = [[1, 34, 5, [2, 4], [[1, 4]]]]
 function flatten(arr) {
-    return [].concat(
-        ...arr.map(item => {
-            return Array.isArray(item) ? flatten(item) : item
-        })
-    )
+  return [].concat(
+    ...arr.map((item) => {
+      return Array.isArray(item) ? flatten(item) : item
+    })
+  )
 }
 const result = flatten(arr)
 console.log(result)
+
+function flatten1(arr) {
+  return arr.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? flatten1(cur) : cur)
+  }, [])
+}
+const result1 = flatten1(arr)
+console.log(result1)
