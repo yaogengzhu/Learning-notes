559b2cba12fd3eecfd9ba3064f3ca12fdabdf1f1
yaogengzhu <455947455@qq.com>
2023-01-29 13:26:12 +0800

    add

--- note/mysql/基础/分组.sql
+++ note/mysql/基础/分组.sql
@@ -4,3 +4,12 @@ SELECT COUNT(*) as 数量, staff  FROM emp WHERE staff = '前端';
 
 # 显示年龄的最大差额
 SELECT Max(age) - MIN(age) from emp;
+
+
+-- GROUP BY *** HAVING *** ORDER BY
+select dept, avg(salary) 
+    from emp 
+    group by dept
+    having avg(salary) > 3000 # 条件
+    order by avg(salary) desc 
+    limit 0, 2;
