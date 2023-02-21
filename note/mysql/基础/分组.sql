32d5556e7dde677d73ccbee577eb152fd7ba4b58
yaogengzhu <455947455@qq.com>
2023-02-21 13:22:37 +0800

    group by | having(条件) ｜ oder by | limit

--- note/mysql/基础/分组.sql
+++ note/mysql/基础/分组.sql
@@ -1,4 +1,5 @@
 SELECT  count(*) as 总数, AVG(age), staff from emp group by staff;
+# 按年龄分组，显示每个年龄段的人数
 SELECT COUNT(age), age  FROM  emp group by age;
 SELECT COUNT(*) as 数量, staff  FROM emp WHERE staff = '前端';
 
@@ -13,3 +14,5 @@ select dept, avg(salary)
     having avg(salary) > 3000 # 条件
     order by avg(salary) desc 
     limit 0, 2;
+
+# 分组查询-- 按照部门分组，显示每个部门的平均工资
